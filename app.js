a86c65691e4904220d52905cda113b9dbc23de7a
Karan Khanna <41183388+karankhanna02@users.noreply.github.com>
2021-01-18 18:37:06 +0530

    Add home page (#1)
    * Add home page
    
    * Revamp home page.

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ app.use(express.static(__dirname + "/Public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.render("index", {name: "Robo"});
+    res.render("index");
 });
 
-app.get("/blogs", function(req, res) {
-    res.send("Blogs");
-});
 
 app.listen(3000, function() {
     console.log("Server is up and running on port 3000.");
